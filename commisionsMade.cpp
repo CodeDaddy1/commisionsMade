9529330f7621b11b3df69eea72b73380ab3e2409
codedaddy1@outlook.com <codedaddy1@outlook.com>
2023-09-27 12:17:57 -0500

    Add project files.

---
+++ commisionsMade.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include <iomanip>
+#include <cmath>
+#include <string>
+
+
+using namespace std;
+
+int main() {
+
+	double commission_Rate;
+	double sales_Amount;
+	double commissions_Made;
+	int commission_Percent;
+
+	cout << "What is your sales amount?\n";
+	cin >> sales_Amount;
+
+
+	cout << setprecision(2) << fixed;
+
+	if (sales_Amount <= 10000) {
+		commission_Rate = .10;
+		commissions_Made = sales_Amount * commission_Rate;
+	}
+
+	else if (sales_Amount > 15000) {
+		commission_Rate = .20;
+		commissions_Made = sales_Amount * commission_Rate;
+	}
+
+	else if (sales_Amount < 15000 && sales_Amount > 10000) {
+		commission_Rate = .15;
+		commissions_Made = sales_Amount * commission_Rate;
+	}
+
+	commission_Percent = commission_Rate * 100;
+
+	cout << "Your commission rate based on your sales is " << commission_Percent << "% \n"
+		"Your total commission made based on your sales is $" << commissions_Made << endl;
+
+	return 0;
+}
